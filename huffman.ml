d4293e8f5d9e26121b8764f0650d5b651487685d
p9romain <romainperrod.pro9@gmail.com>
2023-01-07 21:17:55 +0100

    Get rid of .hf for decompressed files

--- huffman.ml
+++ huffman.ml
@@ -3,17 +3,21 @@ let decompress s =
   let stream_in = Bs.of_in_channel channel_in in
   let tree = Read_write.header_to_tree stream_in in
   let sep = Char.chr 47 in (* char : / *)
-  let l = String.split_on_char sep s in
-  let l = List.rev l in
-  let file = List.hd l in
-  let file = "new_" ^ file in
-  let l = List.rev (file :: List.tl l) in
-  let s_new = String.concat (String.make 1 sep) l in
-  let channel_out = open_out s_new in
-  let _ = Bs.read_n_bits stream_in 23 in
-  let () = Read_write.transcript_body tree stream_in channel_out in
-  let () = close_in channel_in in
-  close_out channel_out
+  let l = String.split_on_char sep s in (*"test_files/name.ext.hf" >> ["test_files" ; "name.ext.hf"]*)
+  let l = List.rev l in (*["name.ext.hf" ; "test_files"]*)
+  let file = List.hd l in (*"name.ext.hf"*)
+  let l_file = String.split_on_char '.' file (*["name" ; "ext" ; "hf"]*) in
+  match l_file with
+  | [] | _ :: [] -> failwith "Given file isn't correct (not name.ext.hf)"
+  | name :: ext :: l_file ->
+    let file = "new_" ^ name ^ "." ^ ext in (*"new_name.ext"*)
+    let l = List.rev (file :: List.tl l) in (*["test_files" ; "new_name.ext"]*)
+    let s_new = String.concat (String.make 1 sep) l in (*"test_files/new_name.ext"*)
+    let channel_out = open_out s_new in
+    let _ = Bs.read_n_bits stream_in 23 in
+    let () = Read_write.transcript_body tree stream_in channel_out in
+    let () = close_in channel_in in
+    close_out channel_out
 
 let compress s = 
   let hash = Read_write.occ_hash s in
@@ -26,7 +30,7 @@ let compress s =
     let ind, _ = Hash_table.find (a, h1, h2) n in
     a.(ind) <- Hash_table.Code (n, bin)
   in
-  let () = List.iter modify l in
+  let () = List.iter modify l in (*on modifie la table de hachage*)
   (*Pas stocker le text dans un string et réécrire petit à petit ?*)
   let s_file = Read_write.file_to_huff_string hash s in
   let sep_header_body = String.make 24 '1' in
